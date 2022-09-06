f12a1bd1d4e26330eb87b1368b4e75993d8263eb
Dave Oyangorin <chocobar12318@gmail.com>
2022-09-06 15:42:46 +0800

    hehe

--- resources/js/Components/Navbar.vue
+++ resources/js/Components/Navbar.vue
@@ -1,15 +1,14 @@
 <template>
-  <nav class="flex bg-primary fixed top-0 left-0 topnav" id="myTopnav"> 
-      <!-- logo -->
-      <a class="active p-button p-button-lg text-white p-button-text font-bold" @click="$inertia.get('/')"><img
-          src="/favicon.ico" alt="Icon" class="mr-2" width="40"> {{ `INTEGRATED HRIS` }}</a>
-      <slot name="links" />
-    <div class="flex flex-1 align-items-center justify-content-end mx-2">
-      <Button href="javascript:void(0);" class="icon" @click="nav_func()" id="buttonNav">
-        <i class="pi pi-bars"></i>
-      </Button>
-      <slot name="right-items" />
-    </div>
+  <nav class="flex bg-primary fixed top-0 left-0 topnav" id="myTopnav">
+    <!-- logo -->
+    <a class="active p-button p-button-lg text-white p-button-text font-bold" @click="$inertia.get('/')"><img
+        src="/favicon.ico" alt="Icon" class="mr-2" width="40"> {{ `INTEGRATED HRIS` }}</a>
+    <slot name="links" />
+    <Button href="javascript:void(0);" class="icon flex flex-1 align-items-center justify-content-end mx-2"
+      @click="nav_func()" id="buttonNav">
+      <i class="pi pi-bars"></i>
+    </Button>
+    <slot name="right-items" />
   </nav>
 </template>
 <script>
