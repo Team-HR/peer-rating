1dee38b455049efcf0b4fc77b0751e67536fdd83
Dave Oyangorin <chocobar12318@gmail.com>
2023-01-30 16:38:53 +0800

    wip:department show empty div when button is not click

--- resources/js/Pages/Departments.vue
+++ resources/js/Pages/Departments.vue
@@ -1,46 +1,70 @@
 <template>
   <auth-layout>
+    <div class="flex align-items-center w-full">
+      <p class="text-5xl w-10">RSM Status</p>
+    </div>
+    <div class="flex justify-content-start gap-3 mb-5">
+      <Dropdown v-model="selectedPeriod" :options="periods" optionLabel="SelectPeriod" placeholder="Select a Period" />
+      <Dropdown v-model="selectedYear" :options="yearPeriod" optionLabel="SelectYear" placeholder="Select a Year" />
+      <Button label="Generate" />
+    </div>
+    <div class="mb-5">
+      <span class="p-input-icon-left">
+        <i class="pi pi-search" />
+        <InputText type="text" placeholder="Search" v-model="filters['departments'].value"/>
+      </span>
+    </div>
     <div>
-      <DataTable :value="products" responsiveLayout="scroll" stripedRows  v-model:selection="selectedDepartment" dataKey="id">
+      <DataTable :value="products" responsiveLayout="scroll" stripedRows v-model:selection="selectedDepartment"
+        dataKey="id" :filters="filters">
         <Column field="departments" header="Departments"></Column>
         <Column field="done" header="Done"></Column>
         <Column selectionMode="multiple" headerStyle="width: 3em"></Column>
-      </DataTable>  
+      </DataTable>
     </div>
   </auth-layout>
 </template> 
 
 <script>
+import { FilterMatchMode } from 'primevue/api';
 import AuthLayout from "@/Layouts/Authenticated";
 import PmsToolbar from "@/Layouts/PmsToolbar";
-// import {ref , onMounted} from 'vue';
 import ProductService from '@/Api/DepartmentsMap';
-import departments from '@/departments'
 
-// export default{
-//   setup(){
-//     onMounted(() => {
-//       productService.value.getProductsSmall().then(data => products.value = data);
-//     })
 
-//     const selectedDepartment = ref();
-//     const products = ref();
-//     const productService = ref(new ProductService());
-
-//     return {products, productService,selectedDepartment}
-    
-//   },
-//   components: {
-//     AuthLayout,
-//     PmsToolbar,
-//   },
-// }
 export default {
   data() {
     return {
-      skills:{},
       selectedDepartment: [],
-      products: null
+      products: null,
+      selectedPeriod: "",
+      selectedYear: "",
+      filters: {},
+      periods: [{
+        SelectPeriod: 'January - June',
+      },
+      {
+        SelectPeriod: 'July - December'
+      }],
+      yearPeriod: [
+        {
+          SelectYear: 2021,
+        },
+        {
+          SelectYear: 2020,
+        },
+        {
+          SelectYear: 2019,
+        },
+        {
+          SelectYear: 2018,
+        },
+      ],
+      initFilters() {
+        this.filters ={
+          'departments': { value: null, matchMode: FilterMatchMode.CONTAINS }
+        }
+      }
     }
   },
   departmentRec: null,
@@ -51,15 +75,14 @@ export default {
   productService: null,
   created() {
     this.productService = new ProductService();
-    // console.log(this.productService.getProductsSmall());
+    this.initFilters();
   },
   mounted() {
-    this.skills = departments.department();
     this.productService.getProductsSmall().then(data => this.products = data);
   },
-  
-  methods: {
 
+  methods: {
   }
+  
 };
 </script>
